7daed8bb0dbd27c66d83d44e25a3be5a19fb30a8
JinYee Jeong <luvdo0112@daum.net>
2024-01-07 21:58:57 +0900

    Fix title, 필요없는 부분 없애기

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,12 @@ import "./App.css";
 import Editor from "./Editor";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <main className="main">
         <section className="memo-pad">
           <Editor></Editor>
         </section>
-
-        <section className="memo-list"></section>
       </main>
     </>
   );
