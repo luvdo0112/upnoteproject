58f28646b3b436d9e4da5e357bc7fff1d1862212
JinYee Jeong <luvdo0112@daum.net>
2024-01-09 22:25:38 +0900

    Add 메모 전체 alert으로 띄우기, 주석 삭제

--- src/component/memoCard/memoCard.tsx
+++ src/component/memoCard/memoCard.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import "./memoCard.css";
 
-interface UserItem {
-  // 여기에 userItems의 각 객체에 들어갈 속성들의 타입을 지정해주세요
-}
-
 const MemoCard: React.FC = () => {
   const [savedUserItems, setSavedUserItems] = useState<UserItem[]>(
     JSON.parse(localStorage.getItem("userItems") || "[]")
   );
+
+  const handleSpanClick = (item: UserItem) => {
+    const texts: string[] = item.root.children.map(
+      (child: any) => child.children[0].text
+    );
+    alert(texts.join(" "));
+  };
 
   const handleMemoDelete = (index: number) => {
     const updatedItems = savedUserItems.filter((_, i) => i !== index);
@@ -20,8 +23,7 @@ const MemoCard: React.FC = () => {
     <ul className="memoCard">
       {savedUserItems.map((item: UserItem, index: number) => (
         <li className="memoCardLi" key={index}>
-          {/* item 객체의 구조에 따라서 각 요소에 직접 접근하여 작업을 수행 */}
-          <span className="memoCardTitle">
+          <span className="memoCardTitle" onClick={() => handleSpanClick(item)}>
             {item.root.children[0].children[0].text}
           </span>
           <button type="button" onClick={() => handleMemoDelete(index)}>
